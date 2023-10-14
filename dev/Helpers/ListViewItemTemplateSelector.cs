4db256c92614b3448958e0b2040adde5fbe54df7
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-10-14 18:55:53 +0330

    Improve UI

---
+++ dev/Helpers/ListViewItemTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿namespace AlAnvar.Common;
+public class ListViewItemTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate QuranTemplate { get; set; }
+    public DataTemplate TranslationTemplate { get; set; }
+
+    protected override DataTemplate SelectTemplateCore(object item)
+    {
+        if (item == null) return null;
+        var search = item as QuranSearch2;
+        var searchOptionIndex = 0;
+        if (MainPage.Instance != null)
+        {
+            searchOptionIndex = MainPage.Instance.GetQuranSearchOptionIndex();
+        }
+
+        if (searchOptionIndex == 0 && search.IsTranslation)
+        {
+            return TranslationTemplate;
+        }
+        else if (searchOptionIndex == 0 && !search.IsTranslation)
+        {
+            return QuranTemplate;
+        }
+        else
+        {
+            return QuranTemplate;
+        }
+    }
+}
