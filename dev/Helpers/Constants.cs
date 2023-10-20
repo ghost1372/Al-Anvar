1c2142ce563e06eab1f835acb72bb2a3c7c857ff
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-10-20 23:52:52 +0330

    Update DB

--- dev/Helpers/Constants.cs
+++ dev/Helpers/Constants.cs
@@ -17,7 +17,7 @@ public static class Constants
     public const double AYAT_DEFAULT_FONT_SIZE = 34;
     public const double AYAT_NUMBER_DEFAULT_FONT_SIZE = 26;
     public const double TRANSLATION_DEFAULT_FONT_SIZE = 20;
-    public const string AYAT_DEFAULT_FONT_NAME = "ms-appx:///Assets/Fonts/UthmanicHafs_v2-1.ttf#KFGQPC HAFS Uthmanic Script";
+    public const string AYAT_DEFAULT_FONT_NAME = "ms-appx:///Assets/Fonts/UthmanTN_v2-0.ttf#KFGQPC Uthman Taha Naskh";
     public const string AYAT_NUMBER_DEFAULT_FONT_NAME = "ms-appx:///Assets/Fonts/Nabi.ttf#Nabi";
     public const string TRANSLATION_DEFAULT_FONT_NAME = "ms-appx:///Assets/Fonts/IRANYekanRegular.ttf#IRANYekan";
 }
