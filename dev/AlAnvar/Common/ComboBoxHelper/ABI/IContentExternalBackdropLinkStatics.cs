415d60201a3d38d58dc6c365525dad5f12ae4ec0
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-01-05 16:29:48 +0330

    Revert "Enable SystemBackdrop for ComboBox"
    This reverts commit 4cf09d276b6fcf363b1cbddadcd6438aadccc06a.

--- dev/AlAnvar/Common/ComboBoxHelper/ABI/IContentExternalBackdropLinkStatics.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Runtime.InteropServices;
-
-namespace ABI.Microsoft.UI.Content
-{
-    [Guid("46CAC6FB-BB51-510A-958D-E0EB4160F678")]
-    internal interface IContentExternalBackdropLinkStatics : global::Microsoft.UI.Content.IContentExternalBackdropLinkStatics;
-}
