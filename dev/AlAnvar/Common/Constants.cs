7dfb9d6b90328d44170cfacc2a9766a7b53c3d26
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-05-22 15:52:39 +0330

    Update Constants.cs

--- dev/AlAnvar/Common/Constants.cs
+++ dev/AlAnvar/Common/Constants.cs
@@ -5,7 +5,7 @@ namespace AlAnvar.Common;
 
 public static partial class Constants
 {
-    public static readonly string RootDirectoryPath = Path.Combine(PathHelper.GetAppDataFolderPath(), ProcessInfoHelper.ProductNameAndVersion);
+    public static readonly string RootDirectoryPath = Path.Combine(PathHelper.GetAppDataFolderPath(), ProcessInfoHelper.ProductName);
     public static readonly string LogDirectoryPath = Path.Combine(RootDirectoryPath, "Log");
     public static readonly string LogFilePath = Path.Combine(LogDirectoryPath, "Log.txt");
     public static readonly string AppConfigPath = Path.Combine(RootDirectoryPath, "AppConfig.json");
